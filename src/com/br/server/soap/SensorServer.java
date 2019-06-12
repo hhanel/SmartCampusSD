db544b7b84b99a85441f43d1f8535f99fb1c461c
WillianMullerUPF <51104786+WillianMullerUPF@users.noreply.github.com>
2019-06-12 14:49:37 -0300

    Update SensorServer.java
    Alterado na interface tipo do dado id de string para inteiro

--- src/com/br/server/soap/SensorServer.java
+++ src/com/br/server/soap/SensorServer.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 
 public interface SensorServer {
 
-    public boolean addSensor(String sensor);
+	public boolean addSensor(String sensor);
 
-    public String addTipoSensor(String tpSensor);
+	public String addTipoSensor(String tpSensor);
 
-    public String deleteSensor(Integer id);
+	public String deleteSensor(Integer id);
 
-    public String deleteTipoSensor(Integer id);
+	public String deleteTipoSensor(Integer id);
 
-    public String[] getSensor(String id);
+	public String[] getSensor(Integer id);
 
-    public String[] getTipoSensor(String id);
+	public String[] getTipoSensor(Integer id);
 
-    public ArrayList<String[]> getAllSensors();
+	public ArrayList<String[]> getAllSensors();
 
-    public ArrayList<String[]> getAllTipoSensors();
+	public ArrayList<String[]> getAllTipoSensors();
 }
